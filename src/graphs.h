d3a5bb5c5f2ec672a6d3fbebcbc46a8a1dd52a3b
Oli_Hydon <oh299@cam.ac.uk>
2023-01-28 21:50:03 +0000

    Delete

--- src/graphs.h
+++
@@ -1,8 +0,0 @@
-#include <Arduino.h>
-
-
-
-int absolute(int x){
-        
-return 10;
-}
--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <Arduino.h>
-#include "graphs.h"
-
-void test_function(){
-  delay(300);
-  Serial.begin(9700);
-  Serial.println(absolute(10));
-}
