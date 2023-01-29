9678a1c84c9e6cf62628fa4448e64aa6136b6c59
Oli_Hydon <oh299@cam.ac.uk>
2023-01-29 14:39:43 +0000

    Added update home page to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
 #include <Arduino.h>
 #include "graphs.h"
 #include <EasyNextionLibrary.h>
+#include "home_page.h"
+
+EasyNex myNex(Serial);
 
 
 void setup(){
-  EasyNex myNex(Serial);
+  myNex.begin(9600);
 }
 
-void test_function(){
-  delay(300);
-  Serial.begin(9700);
-  Serial.println(absolute(10));
+void loop(){
+  myNex.NextionListen();
+  int page = myNex.readNumber("dp");
+  if (page == 1){
+    updateHomePage(myNex,10,10,10,10,10,10,10,10);
+  }
 }
+
